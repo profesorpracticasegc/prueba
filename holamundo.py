726165203b7208ddc1382418313f54e79b84cac6
Profesor EGC <profesorpracticasegc@gmail.com>
2017-11-16 16:57:22 +0100

    Añade un segundo parámetro a holamundo

--- holamundo.py
+++ holamundo.py
@@ -1,9 +1,9 @@
-def holamundo(mensaje):
-	print "hola mundo " + mensaje
+def holamundo(mensaje, x):
+	print "hola mundo " + mensaje + x
 
 def adiosmundo():
 	print "adios mundo"
 
 mensaje = "este es un mensaje de prueba, mensaje, 1 2 3, probando"
 holamundo()
-adiosmundo(mensaje)
+adiosmundo(mensaje, x)
