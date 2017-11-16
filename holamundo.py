12a62d379f95bc0babffded33041a44e95635115
Profesor EGC <profesorpracticasegc@gmail.com>
2017-11-16 17:00:25 +0100

    imprimir mensaje dependiente de valor
    Este valor se toma como parámetro de la función holamundo.

--- holamundo.py
+++ holamundo.py
@@ -1,9 +1,13 @@
-def holamundo(mensaje, x):
+def holamundo(mensaje, x, z):
+	if z < 10:
+		print "el valor %d es mas pequeno que 10" %(z)
+
 	print "hola mundo " + mensaje + x
 
 def adiosmundo():
 	print "adios mundo"
 
 mensaje = "este es un mensaje de prueba, mensaje, 1 2 3, probando"
-holamundo()
-adiosmundo(mensaje, x)
+holamundo(mensaje, "prueba", 8)
+adiosmundo()
+
