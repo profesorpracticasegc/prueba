f058d8e24ddbc7fcd67cf4fd8858459ea8d750af
Profesor EGC <profesorpracticasegc@gmail.com>
2017-11-16 16:50:57 +0100

    Modifica el mensaje de hola mundo

--- holamundo.py
+++ holamundo.py
@@ -4,6 +4,6 @@ def holamundo(mensaje):
 def adiosmundo():
 	print "adios mundo"
 
-mensaje = "este es un mensaje de prueba"
+mensaje = "este es un mensaje de prueba, mensaje, 1 2 3, probando"
 holamundo()
 adiosmundo(mensaje)
