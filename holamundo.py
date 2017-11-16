2bc845a267d4f5ba6b776a1f2c71f78a9b0df228
Profesor EGC <profesorpracticasegc@gmail.com>
2017-11-16 16:43:16 +0100

    pasa parámetro a adiosmundo()
    Como ejemplo.

--- holamundo.py
+++ holamundo.py
@@ -1,8 +1,9 @@
-def holamundo():
-	print "hola mundo"
+def holamundo(mensaje):
+	print "hola mundo " + mensaje
 
 def adiosmundo():
 	print "adios mundo"
 
+xx = "mensaje"
 holamundo()
-adiosmundo(),
+adiosmundo(xx)
